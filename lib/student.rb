a76e825ff33ced8b5db5c5dc6c5ca7aaf225a2d8
Adriana DiPietro <am20dipi@alum.siena.edu>
2020-10-11 22:07:29 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -3,9 +3,9 @@ require_relative "../config/environment.rb"
 class Student
   attr_accessor :name, :grade, :id
 
-  def initialize(attribute_hash, id=nil)
-    @name = attribute_hash[:name]
-    @grade = attribute_hash[:grade]
+  def initialize(name, grade, id=nil)
+    @name = name
+    @grade = grade
     @id = id
   end
   
